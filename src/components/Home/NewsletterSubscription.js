d9e09d2e3fe0670795ccc4e1808d0786474f0bc2
AhmadyarRashid <ahmadyarmeo@gmail.com>
2021-05-19 22:37:04 +0500

    display newletter subscription form in row

--- src/components/Home/NewsletterSubscription.js
+++ src/components/Home/NewsletterSubscription.js
@@ -16,13 +16,19 @@ const useStyles = makeStyles((theme) => {
         padding: "40px 0",
       },
       display: 'flex',
+      flexDirection: "column",
       justifyContent: 'center',
     },
     form: {
       display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+      flexDirection: 'row',
+      alignItems: 'baseline',
+      width: '60%',
+      marginTop: 12,
+      justifyContent: "center",
       [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column',
+        alignItems: "center",
         width: '80%'
       },
     },
@@ -33,18 +39,22 @@ const useStyles = makeStyles((theme) => {
       },
     },
     input: {
-      marginTop: 20,
-      minWidth: "100%",
+      minWidth: "40%",
+      marginRight: 12,
+      display: 'flex',
+      flexDirection: 'column',
       [theme.breakpoints.down('sm')]: {
+        marginRight: 0,
+        marginTop: 20,
         padding: 0,
         minWidth: "80%",
       },
     },
     btn: {
-      padding: 8,
-      marginTop: 20,
-      minWidth: "50%",
+      padding: 12,
+      minWidth: "10%",
       [theme.breakpoints.down('sm')]: {
+        marginTop: 20,
         padding: 4,
         minWidth: "40%",
       },
@@ -58,83 +68,86 @@ function NewsLettterSubscription({themeMode}) {
   return (
     <Box component="div" className={classes.root}
          style={{backgroundColor: themeMode === "light" ? "white" : colors.black1}}>
-      <Formik
-        initialValues={{email: ''}}
-        validate={values => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, {setSubmitting, resetForm}) => {
-          setTimeout(() => {
-            axios.post(`${baseUrl}/api/event/subscription`, {
-              email: values.email
-            }).then(({data}) => {
-              if (data.isSuccess) {
-                resetForm({})
+      <center>
+        <Formik
+          initialValues={{email: ''}}
+          validate={values => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, {setSubmitting, resetForm}) => {
+            setTimeout(() => {
+              axios.post(`${baseUrl}/api/event/subscription`, {
+                email: values.email
+              }).then(({data}) => {
+                if (data.isSuccess) {
+                  resetForm({})
+                  Swal.fire({
+                    icon: 'success',
+                    title: data.payload,
+                    timer: 1500
+                  })
+                } else {
+                  Swal.fire({
+                    icon: 'info',
+                    title: data.message,
+                    timer: 1500
+                  })
+                }
+              }).catch(error => {
                 Swal.fire({
-                  icon: 'success',
-                  title: data.payload,
-                  timer: 1500
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Something went wrong.',
                 })
-              } else {
-                Swal.fire({
-                  icon: 'info',
-                  title: data.message,
-                  timer: 1500
-                })
-              }
-            }).catch(error => {
-              Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong.',
               })
-            })
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-          <form className={classes.form} onSubmit={handleSubmit}>
-            <Typography variant="h4" className={classes.title}>Newsletter Subscription</Typography>
-            <OutlinedInput
-              placeholder="enter email"
-              variant="outlined"
-              className={classes.input}
-              error={errors.email && touched.email && errors.email}
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            <span style={{color: 'red', fontSize: 11}}>{errors.email && touched.email && errors.email}</span>
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              variant="contained"
-              color="primary"
-              className={classes.btn}
-            >Subscribe</Button>
-          </form>
-        )}
-      </Formik>
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+            <form className={classes.form} onSubmit={handleSubmit}>
+              <Typography variant="h6" align="center" className={classes.title}>Newsletter Subscription</Typography>
+              <Box component="div" className={classes.input}>
+                <OutlinedInput
+                  placeholder="enter email"
+                  variant="outlined"
+                  error={errors.email && touched.email && errors.email}
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <span style={{color: 'red', fontSize: 11}}>{errors.email && touched.email && errors.email}</span>
+              </Box>
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                variant="contained"
+                color="primary"
+                className={classes.btn}
+              >Subscribe</Button>
+            </form>
+          )}
+        </Formik>
+      </center>
     </Box>
   )
 }
